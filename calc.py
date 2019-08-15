a2e594cfcb4b035662e8ed3a39c87adcd395c007
MrSedan <elpashevs@bk.ru>
2019-08-15 23:54:47 +0300

    test

--- calc.py
+++ calc.py
@@ -1,2 +1,18 @@
-a = [i+1 for i in range(5)]
-# dzfhvbklsdfb
+from gui import *
+
+def bp():
+    text = ui.lineEdit.text()
+    try:
+        ui.lineEdit.setText(text[:len(text)-1])
+    except:
+        pass
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    ui.pushButton_17.clicked.connect(bp)
+    sys.exit(app.exec_())
